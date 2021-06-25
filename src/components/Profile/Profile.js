0898c77a039ef748fc87c914b4eab31a046e2a34
mabvmex <mabvmex@gmail.com>
2021-06-25 15:31:53 -0500

    info de perfil

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,55 @@
 import React from "react"
+import { Container, Row, Col, Image } from "react-bootstrap"
+import profileAvatar from "../../images/jpg/avatar.jpg"
 import "./Profile.scss"
+
+const data = [
+  {
+    title: "Edad:",
+    info: "33 años",
+  },
+  {
+    title: "Dirección:",
+    info: "México",
+  },
+  {
+    title: "Teléfono:",
+    info: "(+52) 55 5555555",
+  },
+  {
+    title: "e-mail::",
+    info: "mabvmex@gmail.com",
+  },
+]
 
 export default function Profile() {
   return (
     <div className="profile">
       <div className="wallpaper" />
       <div className="dark" />
+      <Container className="box">
+        <Row className="info">
+          <Col xs={12} md={4}>
+            <Image src={profileAvatar} fluid />
+          </Col>
+
+          <Col xs={12} md={8} className="info__data">
+            <span> Info </span>
+            <p> Miguel Barrera </p>
+            <p> FullStack web developer </p>
+            <hr />
+
+            <div className="more-info">
+              {data.map((item, index) => (
+                <div key={index} className="item">
+                  <p> {item.title} </p>
+                  <p> {item.info} </p>
+                </div>
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
