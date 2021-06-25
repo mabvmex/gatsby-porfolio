28438cf02831919f02062b406b67c7faf454b417
mabvmex <mabvmex@gmail.com>
2021-06-24 22:22:04 -0500

    componente profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,8 @@ import "./Profile.scss"
 export default function Profile() {
   return (
     <div className="profile">
-      <div className="wallpaper">::: Componenete Profile:::</div>
+      <div className="wallpaper" />
+      <div className="dark" />
     </div>
   )
 }
