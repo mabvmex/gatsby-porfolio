743cc30d99e07f43c473d90bd677ca3a980a270b
mabvmex <mabvmex@gmail.com>
2021-06-24 21:47:00 -0500

    componentes

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Profile.scss"
+
+export default function Profile() {
+  return (
+    <div className="profile">
+      <div className="wallpaper">::: Componenete Profile:::</div>
+    </div>
+  )
+}
